f5a997ca0be3b529e38f4e94f531f397abf89059
Daniel Palmer <daniel@thingy.jp>
2026-01-14 22:38:31 +0900

    init: Just enough to repeatedly start /bin/sh

---
+++ init.c
@@ -0,0 +1,24 @@
+int main (int argc, char **argv, char **envp)
+{
+	printf("smol init\n");
+
+	while (true) {
+		pid_t pid = vfork();
+
+		/* We are init */
+		if (pid) {
+			wait(NULL);
+		}
+		/* We are the new process */
+		else {
+			static char *newargv[] = { "sh", NULL };
+			static char *newenviron[] = { NULL };
+
+			execve("/bin/sh", newargv, newenviron);
+			printf("execve failed\n");
+			break;
+		}
+	}
+
+	return 0;
+}
