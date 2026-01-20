7ab6cb6ca57ba6089603c007309e9509fcdf95f6
Daniel Palmer <daniel@thingy.jp>
2026-01-20 22:33:38 +0900

    ls: Worst version of ls ever

---
+++ ls.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+int main (int argc, char **argv, char **envp)
+{
+	DIR *dir;
+	struct dirent e, *result;
+	char *path = ".";
+
+	if (argc == 2)
+		path = argv[1];
+
+	dir = opendir(path);
+	if (!dir)
+		return 1;
+
+	while (readdir_r(dir, &e, &result) == 0 && result)
+		printf("%s\n", e.d_name);
+
+	closedir(dir);
+
+	return 0;
+}
