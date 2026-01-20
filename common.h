0d100e57958e5fe2effa97c931e000af0135c7bd
Daniel Palmer <daniel@thingy.jp>
2026-01-21 07:06:06 +0900

    init: Use a loop to do the "fstab" part

---
+++ common.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _SMOLUTILS_COMMON_H
+#define _SMOLUTILS_COMMON_H
+
+#define ARRAY_SIZE(_a) (sizeof(_a) / sizeof(_a[0]))
+
+#endif
--- init.c
+++ init.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-/* This avoids having to use a mount command, fstab etc */
+#include "common.h"
+
 static int do_mount(const char *source, const char *target, const char *type)
 {
 	int ret;
@@ -12,19 +13,32 @@ static int do_mount(const char *source, const char *target, const char *type)
 	return ret;
 }
 
+struct mountpoint {
+	const char *source;
+	const char *target;
+	const char *type;
+};
+
+static const struct mountpoint fstab[] = {
+	{"sysfs", "/sys", "sysfs"},
+	{"proc", "/proc", "proc"},
+	{"tmp", "/tmp", "tmpfs"},
+};
+
+/* This avoids having to use a mount command, fstab etc */
 static int mount_filesystems(void)
 {
 	int ret;
 
 	printf("mounting filesystems...\n");
 
-	ret = do_mount("sysfs", "/sys", "sysfs");
-	if (ret)
-		goto err;
+	for (int i = 0; i < ARRAY_SIZE(fstab); i++) {
+		const struct mountpoint *mp = &fstab[i];
 
-	ret = do_mount("proc", "/proc", "proc");
-	if (ret)
-		goto err;
+		ret = do_mount(mp->source, mp->target, mp->type);
+		if (ret)
+			goto err;
+	}
 
 	return 0;
 
