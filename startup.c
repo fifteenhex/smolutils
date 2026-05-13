d4efe391c3f31d1828c7803c1e630e07ee343c68
Daniel Palmer <daniel@thingy.jp>
2026-05-13 22:37:37 +0900

    init/startup: Move one time filesystem mount code into startup to shrink init

--- startup.c
+++ startup.c
@@ -3,7 +3,57 @@
 #include "config.h"
 #include "common.h"
 
+static int do_mount(const char *source, const char *target, const char *type)
+{
+	int ret;
+
+	ret = mount(source, target, type, 0, NULL);
+	if (ret) {
+		error("mount(%s) failed: %d\n", target, ret);
+		return ret;
+	}
+
+	debug("mounted %s(%s) on %s\n", source, type, target);
+
+	return 0;
+}
+
+struct mountpoint {
+	const char *source;
+	const char *target;
+	const char *type;
+};
+
+static const struct mountpoint fstab[] = {
+	{"sysfs", "/sys", "sysfs"},
+	{"proc", "/proc", "proc"},
+	{"tmp", "/tmp", "tmpfs"},
+};
+
+/* This avoids having to use a mount command, fstab etc */
+static int mount_filesystems(void)
+{
+	int ret;
+
+	debug("mounting filesystems...\n");
+
+	for (int i = 0; i < ARRAY_SIZE(fstab); i++) {
+		const struct mountpoint *mp = &fstab[i];
+
+		ret = do_mount(mp->source, mp->target, mp->type);
+		if (ret)
+			goto err;
+	}
+
+	return 0;
+
+err:
+	return ret;
+}
+
 int main (int argc, char **argv, char **envp)
 {
+	mount_filesystems();
+
 	return 0;
 }
