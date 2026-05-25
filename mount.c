4538b3002004764d34d38e46b817f051867e127f
Daniel Palmer <daniel@thingy.jp>
2026-05-26 07:04:40 +0900

    mount: Now a barely functional mount. I thought about using the new mount api but I am too lazy

--- mount.c
+++ mount.c
@@ -5,5 +5,36 @@
 
 int main (int argc, char **argv, char **envp)
 {
+	int ret;
+	char *source = NULL;
+	char *target = NULL;
+	char *type = NULL;
+	char c;
+
+        while ((c = getopt(argc, argv, "t:")) != -1) {
+                switch (c) {
+                case 't':
+			type = optarg;
+                        break;
+                }
+        }
+
+	if (!type)
+		return 1;
+
+	source = (optind < argc) ? argv[optind++] : NULL;
+	if (!source)
+		return 1;
+
+	target = (optind < argc) ? argv[optind++] : NULL;
+	if (!target)
+		return 1;
+
+	ret = mount(source, target, type, 0, NULL);
+	if (ret) {
+		error("mount(%s) failed: %d\n", target, ret);
+		return 1;
+	}
+
 	return 0;
 }
