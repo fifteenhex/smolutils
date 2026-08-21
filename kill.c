7c29fcb0cc7d3a11ace0c3ee81bd76231b8766ed
Daniel Palmer <daniel@thingy.jp>
2026-08-21 23:47:17 +0900

    Fill in kill

--- kill.c
+++ kill.c
@@ -1,8 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "config.h"
+#include "common.h"
+
 int main (int argc, char **argv, char **envp)
 {
-	kill(1, SIGUSR1);
+	int sig = SIGTERM;
+	char *end;
+	long pid;
+	int i = 1;
+
+	if (argc > 1 && argv[1][0] == '-') {
+		sig = strtol(argv[1] + 1, &end, 10);
+		if (*end != '\0')
+			return 1;
+		i++;
+	}
+
+	if ((argc - i) != 1)
+		return 1;
+
+	pid = strtol(argv[i], &end, 10);
+	if (*end != '\0' || pid < 1)
+		return 1;
+
+	if (kill(pid, sig)) {
+		error("kill() failed: %d\n", errno);
+		return 1;
+	}
 
 	return 0;
 }
