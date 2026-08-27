1aef943d04c81609dc83eaf8c156c5e83895cfa9
Daniel Palmer <daniel@thingy.jp>
2026-08-27 22:43:49 +0900

    Implement chmod

--- chmod.c
+++ chmod.c
@@ -5,5 +5,29 @@
 
 int main (int argc, char **argv, char **envp)
 {
-	return 0;
+	unsigned long mode;
+	char *end;
+	int ret = 0;
+	int i;
+
+	if (argc < 3) {
+		usage("usage: chmod <octal mode> <file>...\n");
+		return 1;
+	}
+
+	/* Only the octal form, nobody needs u+x that badly */
+	mode = strtoul(argv[1], &end, 8);
+	if (end == argv[1] || *end != '\0' || mode > 07777) {
+		error("Not a mode: %s\n", argv[1]);
+		return 1;
+	}
+
+	for (i = 2; i < argc; i++) {
+		if (chmod(argv[i], mode)) {
+			error("chmod(%s) failed: %d\n", argv[i], errno);
+			ret = 1;
+		}
+	}
+
+	return ret;
 }
