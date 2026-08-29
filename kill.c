360b565f2d66f3dff05309b48635b67b08780e5f
Daniel Palmer <daniel@thingy.jp>
2026-08-29 12:52:36 +0900

    kill: Add usage, be stricter on the pid, avoid truncation etc

--- kill.c
+++ kill.c
@@ -17,12 +17,22 @@ int main (int argc, char **argv, char **envp)
 		i++;
 	}
 
-	if ((argc - i) != 1)
+	if ((argc - i) != 1) {
+		usage("usage: kill [-<signal>] <pid>\n");
 		return 1;
+	}
 
 	pid = strtol(argv[i], &end, 10);
-	if (*end != '\0' || pid < 1)
+
+	/*
+         * pid_t is 32 bits but the value we just parsed might get
+         * truncated resulting in badness so we do a cast to check
+         * for truncation.
+         */
+	if (*end != '\0' || pid < 1 || (long) (pid_t) pid != pid) {
+		error("Not a pid: %s\n", argv[i]);
 		return 1;
+	}
 
 	if (kill(pid, sig)) {
 		error("kill() failed: %d\n", errno);
