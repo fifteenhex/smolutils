9893002586c96cf552f7529f41073d18fdab0ee6
Daniel Palmer <daniel@thingy.jp>
2026-08-29 23:15:09 +0900

    cat: Allow reading stdin, multiple files like normal cat

--- cat.c
+++ cat.c
@@ -3,31 +3,47 @@
 #include "config.h"
 #include "common.h"
 
-int main (int argc, char **argv, char **envp)
+static int cat_fd(int fd)
 {
 	char buf[4096];
-	char *path;
 	int len;
-	int fd;
-
-	if (argc != 2)
-		return 1;
-
-	path = argv[1];
-
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-		return 1;
 
 	while (true) {
 		len = read(fd, buf, sizeof(buf));
-		if (len <= 0)
-			break;
+		if (len < 0)
+			return -1;
+		if (len == 0)
+			return 0;
+
 		/* FIXME: For now write byte by byte because of nolibc's fwrite() */
 		fwrite(buf, 1, len, stdout);
 	}
+}
 
-	close(fd);
+int main (int argc, char **argv, char **envp)
+{
+	int ret = 0;
+	int i;
 
-	return 0;
+	/* No arguments means read stdin */
+	if (argc < 2)
+		return cat_fd(STDIN_FILENO) ? 1 : 0;
+
+	for (i = 1; i < argc; i++) {
+		int __cleanup_fd fd = -1;
+
+		fd = open(argv[i], O_RDONLY);
+		if (fd < 0) {
+			error("Failed to open %s: %d\n", argv[i], errno);
+			ret = 1;
+			continue;
+		}
+
+		if (cat_fd(fd)) {
+			error("Failed to read %s: %d\n", argv[i], errno);
+			ret = 1;
+		}
+	}
+
+	return ret;
 }
