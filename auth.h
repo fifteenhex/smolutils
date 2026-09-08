c386cbd31d896e817cb7cfb166f1a03cfa9df9f5
Daniel Palmer <daniel@thingy.jp>
2026-09-08 20:25:31 +0900

    auth: Print stupid OTP

--- auth.h
+++ auth.h
@@ -3,6 +3,9 @@
 #ifndef _SMOLUTILS_AUTH_H
 #define _SMOLUTILS_AUTH_H
 
+#define AUTH_CODE_BYTES	3
+#define AUTH_CODE_LEN	(AUTH_CODE_BYTES * 2)
+
 static bool auth_is_secure(int fd)
 {
 	struct stat here, there;
@@ -11,6 +14,26 @@ static bool auth_is_secure(int fd)
 		return false;
 
 	return S_ISCHR(here.st_mode) && here.st_rdev == there.st_rdev;
+}
+
+static int auth_say(const char *who, const char *code)
+{
+	char line[AUTH_CODE_LEN + 96];
+	int len, fd;
+
+	fd = open(SMOL_SECURETTY_PATH, O_WRONLY | O_NOCTTY);
+	if (fd < 0)
+		return -1;
+
+	len = snprintf(line, sizeof(line), "\r\n%s joined the party, code %s\r\n",
+		       who, code);
+
+	if (len > 0)
+		write_full(fd, line, len);
+
+	close(fd);
+
+	return 0;
 }
 
 #endif  /* _SMOLUTILS_AUTH_H */
