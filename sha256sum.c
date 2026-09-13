9979ebb799965051c333cab6769b8845c3f40e73
Daniel Palmer <daniel@thingy.jp>
2026-09-13 19:31:39 +0900

    sha256sum: Only allow regular files to be hashed

--- sha256sum.c
+++ sha256sum.c
@@ -7,12 +7,16 @@
 #include "thirdparty/lonesha256.h"
 
 static int fd;
+static off_t hashed;
 
-static int read_block(unsigned char* in)
+static int read_block(unsigned char *in)
 {
-	int len = read(fd, in, LSHA256BLKSIZE);
-	if (len < 0)
+	int got = read_full(fd, in, LSHA256BLKSIZE);
+
+	if (got < 0)
 		return 1;
+
+	hashed += got;
 
 	return 0;
 }
@@ -20,8 +24,8 @@ static int read_block(unsigned char* in)
 int main (int argc, char **argv, char **envp)
 {
 	unsigned char out[32] = {0};
+	struct stat st;
 	char *path;
-	off_t filesz;
 
 	if (argc != 2)
 		return 1;
@@ -34,13 +38,20 @@ int main (int argc, char **argv, char **envp)
 		return 1;
 	}
 
-	filesz = file_size(fd);
-	if (filesz < 0) {
+	if (fstat(fd, &st)) {
 		error("Failed to size %s: %d\n", path, errno);
 		return 1;
 	}
 
-	lonesha256_stream(out, read_block, filesz);
+	if (!S_ISREG(st.st_mode)) {
+		error("%s isn't a regular file\n", path);
+		return 1;
+	}
+
+	if (lonesha256_stream(out, read_block, st.st_size)) {
+		error("Failed to read %s: %d\n", path, errno);
+		return 1;
+	}
 
 	for (int i = 0; i < sizeof(out); i++)
 		printf("%02x", (unsigned) out[i]);
